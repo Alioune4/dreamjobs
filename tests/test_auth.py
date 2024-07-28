864933cce81b4c4c999aa7b3965e04b98dd752fd
Alioune4 <papalioune1@gmail.com>
2024-07-28 19:59:07 +0000

    test: fix test by using only client fixtures instead of both client and db fixtures. client fixture already contains db fixture

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,17 +1,16 @@
 import json
 import pytest
 
-def test_register(test_client, test_db):
+def test_register(test_client):
     response = test_client.post('/api/register', json={
         'username': 'testuser',
         'password': 'testpassword',
         'email': 'testuser@example.com'
     })
     assert response.status_code == 201
-    assert 'access_token' in json.loads(response.data)
 
 
-def test_register_login(test_client, test_db):
+def test_register_login(test_client):
     test_client.post('/api/register', json={
         'username': 'testuser',
         'password': 'testpassword',
